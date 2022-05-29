aaceb008a0d556075d3a29261e6c7ca216cdbb36
abdul <abdulramiz007@gmail.com>
2022-05-29 20:10:48 +0530

    abdul

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CountdownTimer from './Components/CountdownTimer/CountdownTimer';
+
+function App() {
+  return (
+    <div className="App">
+     <CountdownTimer
+     countdownTimestampMs={1659983662000}/>
+    </div>
+  );
+}
+
+export default App;
